7f7a6fabb8a9034638cd802446903ecd928da82b
JoshEhrlich <joshehrlich11@gmail.com>
2022-03-30 15:23:57 -0400

    Loading files and visualizing data

--- Open Model.py
+++ Open Model.py
@@ -1,5 +1,28 @@
-import pickle
+from sklearn import datasets
+from sklearn import svm    			
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-filename = "/Users/JoshEhrlich/OneDrive - Queen's University/School/University/MSc/PicoscopeAnalysis/ModelPicoscope/cauteryModelSVM.sav"
-loaded_module = pickle.load(open(filename, "rb")
+X = np.load("/Users/JoshEhrlich/OneDrive - Queen's University/School/University/MSc/PicoscopeAnalysis/OldWork/x.npy")
+#Y = np.load("/Users/JoshEhrlich/OneDrive - Queen's University/School/University/Graduate School/Pico Export Data/sample_data/y.npy")
 
+sample1 = X[2]
+print(X.shape)
+print(Y.shape)
+time1 = sample1[1]
+volt1 = sample1[0]
+
+
+sample1 = X[300]
+time2 = sample1[1]
+volt2 = sample1[0]
+
+# plt.plot(time1, volt1)
+# plt.show()
+
+# plt.plot(time2, volt2)
+# plt.show()
+
+X_new = np.array([])
+Y_new = np.array([])
